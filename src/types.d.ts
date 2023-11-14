c3805fcda9ff54f3b985c868852d671f07da298f
dannagonim <dannagonim@gmail.com>
2023-11-14 14:29:23 -0300

    Initial project setup with some components

---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+//el .d es para tener en cuenta q son las declaraciones, q no va a haber codigo 
+
+export interface Todo {
+    id: string
+    title: string
+    completed: boolean
+}
+  
+export type ListOfTodos = Todo[]
+  
+
+
